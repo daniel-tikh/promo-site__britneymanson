557d78f8360873b3b42b829eb935df2a1592bcea
Daniel Tikhonov <daniel.tihonov@gmail.com>
2024-03-29 23:41:46 +0100

    Finished work

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # promo-site__britneymanson
-Landing pages &amp; promo website 💎 The project was realized completely by me
+LANDING PAGES &amp; PROMO WEBSITE 💎 THE PROJECT WAS REALIZED COMPLETELY BY ME
+
+<a href="//britneymanson.com/" target="_blank" rel="nofollow">britneymanson.com</a>
+
+The internet sensation, runway queen Britney Manson went viral and gained a big audience for a short time. Her first video "walking till I get noticed by a model agency" gained 40M on tiktok and insane comment activity. Estonian born, she already worked for the big brands like Valentino, Etro and has debuted in Berlin and Milan fashion week AW24 season. Britney is a proud trans woman, she represents trans people with all her passion, talking about trans*rights in her social media.
---
+++ index.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<!-- (c) studio "EVERNICE" | http://www.evernice.ru -->
+<html lang="ru">
+
+<head>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
+
+<link rel="preload" href="/include/css/styles.css" as="style">
+<link rel="preload" href="/include/js/jquery.js" as="script">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<title>Britney Manson – official website</title>
+
+<meta name="description" content="Britney Manson, official website. Music, videos, concert schedules.">
+
+<link rel="stylesheet" href="/include/css/styles.css">
+
+<link rel="shortcut icon" href="/images/icons/favicon.png">
+
+<meta property="og:image" content="http://britneymanson.com/images/britneymanson.jpg">
+<meta property="og:title" content="Official website of Britney Manson">
+<meta property="og:description" content="Hi, I am Britney Manson!">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+
+  <div id="en_preloader"><div class="en_preloader__svg"></div></div>
+
+  <header class="en_header">
+
+    <div class="en_header__content">
+
+      <nav class="en_nav">
+
+        <ul class="en_menu">
+
+          <li class="en_menu__item"><p class="en_menu__item__title"><a class="en_menu__item__link" href="#about" title="">About</a></p></li>
+
+          <li class="en_menu__item"><p class="en_menu__item__title"><a class="en_menu__item__link" href="#releases" title="">Releases</a></p></li>
+
+          <li class="en_menu__item"><p class="en_menu__item__title"><a class="en_menu__item__link" href="#merch" title="">Merch</a></p></li>
+
+          <li class="en_menu__item"><p class="en_menu__item__title"><a class="en_menu__item__link" href="#kontakty" title="">Contacts</a></p></li>
+
+        </ul>
+
+      </nav>
+
+      <div class="en_socseti">
+
+        <a class="en_socseti__link tiktok_negro" href="https://tiktok.com/@britneymnsn" target="_blank" title=""></a>
+
+        <a class="en_socseti__link instagram_negro" href="https://instagram.com/britneymnsn" target="_blank" title=""></a>
+
+        <a class="en_socseti__link youtube_negro" href="https://youtube.com/@britneymanson." target="_blank" title=""></a>
+
+      </div>
+
+    </div>
+
+    <div class="en_header__logo"><img src="/images/logo.svg" alt=""></div>
+
+    <picture class="en_header__foto">
+      <source type="image/webp" media="(min-width: 1025px)" srcset="/images/foto_title_1600.webp">
+      <source type="image/webp" media="(min-width: 601px)" srcset="/images/foto_title_1024.webp">
+      <source type="image/webp" srcset="/images/foto_title_600.webp">
+      <img src="/images/foto_title_1600.jpg" loading="lazy" alt="">
+    </picture>
+
+  </header>
+  
+  <main class="en_content">
+
+    <div id="about" class="en_content__about bg_frontera_dos">
+
+      <div class="en_content__about__content">
+
+        <div class="en_content__about__text">
+        
+          <h1 class="en_content__title">Britney</h1>
+          
+          <p class="en_content__about__text__text">She could recreate any supermodel walk, iconic runway moment or fashion show in public. Her catwalk is definitely an eyecatcher. She brings high fashion energy into the city streets with her 2010's Model vibes. And you can recognize her immediately - she's extremely tall, with long blond hair, wearing extra high heels, black Smokey eyes and huge sunglasses.</p>
+
+          <p class="en_content__about__text__quote">Walking like a supermodel around the Internet: the new social media icon Britney Manson.</p>
+          
+          <p class="en_content__about__text__text">The internet sensation, runway queen Britney Manson went viral and gained a big audience for a short time. Her first video "walking till I get noticed by a model agency" gained 40M on tiktok and insane comment activity. Estonian born, she already worked for the big brands like Valentino, Etro and has debuted in Berlin and Milan fashion week AW24 season. Britney is a proud trans woman, she represents trans people with all her passion, talking about trans*rights in her social media. She started her transition in middle school, and this fact made her childhood a hardest time in her life. She has to be bullied, beaten, but this experience made her stronger. Got her first runway experience at 12yo, she always knew who she wants to become.</p>
+        
+          <div class="en_content__about__socseti">
+
+            <a class="en_content__about__socset__link tiktok_negro" href="https://tiktok.com/@britneymnsn" target="_blank" title=""></a>
+
+            <a class="en_content__about__socset__link instagram_negro" href="https://instagram.com/britneymnsn" target="_blank" title=""></a>
+
+            <a class="en_content__about__socset__link youtube_negro" href="https://youtube.com/@britneymanson." target="_blank" title=""></a>
+
+          </div>
+
+        </div>
+      
+      </div>
+        
+      <div class="en_content__about__bg">
+
+        <picture class="en_content__about__bg__foto">
+          <source type="image/webp" media="(min-width: 1201px)" srcset="/images/foto_about_470.webp">
+          <source type="image/webp" media="(min-width: 1025px)" srcset="/images/foto_about_380.webp">
+          <source type="image/webp" media="(min-width: 769px)" srcset="/images/foto_about_320.webp">
+          <source type="image/webp" media="(min-width: 531px)" srcset="/images/foto_about_260.webp">
+          <source type="image/webp" srcset="/images/foto_about_220.webp">
+          <img src="/images/foto_about_470.jpg" loading="lazy" alt="">
+        </picture>
+      
+      </div>
+
+    </div>
+    
+    <div id="releases" class="en_content__releases bg_frontera_uno">
+    
+      <div class="en_content__releases__content">
+
+        <div class="en_content__releases__text">
+        
+          <h1 class="en_content__title white">Releases</h1>
+      
+          <p class="en_content__releases__text__text">Also Britney makes music - her debut track "MODE-L" was written about the dark side of the fashion industry, inspired by her own experience and dedicated to one of her childhood icons, trans*model Andreja Pejic.</p>
+          
+        </div>
+        
+        <div class="en_content__releases__list">
+      
+          <div class="en_content__releases__music">
+
+            <div class="en_content__releases__slogan"><p>Music</p></div>
+
+            <div class="en_content__releases__items">
+            
+              <div id="en_music">
+
+                <div class="slick-slid">
+
+                  <a class="en_content__releases__anuncio" href="https://orcd.co/fcknstr" target="_blank" title="">
+
+                    <div class="en_content__releases__heading">
+
+                      <picture class="en_content__releases__foto music">
+                        <source type="image/webp" media="(min-width: 1401px)" srcset="/images/singl_fcknstr_380.webp">
+                        <source type="image/webp" media="(min-width: 1201px)" srcset="/images/singl_fcknstr_400.webp">
+                        <source type="image/webp" media="(min-width: 1025px)" srcset="/images/singl_fcknstr_320.webp">
+                        <source type="image/webp" media="(min-width: 769px)" srcset="/images/singl_fcknstr_350.webp">
+                        <source type="image/webp" media="(min-width: 461px)" srcset="/images/singl_fcknstr_470.webp">
+                        <source type="image/webp" srcset="/images/singl_fcknstr_400.webp">
+                        <img src="/images/singl_fcknstr.jpg" loading="lazy" alt="">
+                      </picture>
+
+                      <!--p class="en_content__releases__title">FCKNSTR</p-->
+
+                      <p class="en_content__releases__subtitle">single &bull; 2023</p>
+
+                    </div>
+
+                  </a>
+
+                </div>
+
+                <div class="slick-slid">
+
+                  <a class="en_content__releases__anuncio" href="https://orcd.co/bm_fashion" target="_blank" title="">
+
+                    <div class="en_content__releases__heading">
+
+                      <picture class="en_content__releases__foto music">
+                        <source type="image/webp" media="(min-width: 1401px)" srcset="/images/singl_Fashion_380.webp">
+                        <source type="image/webp" media="(min-width: 1201px)" srcset="/images/singl_Fashion_400.webp">
+                        <source type="image/webp" media="(min-width: 1025px)" srcset="/images/singl_Fashion_320.webp">
+                        <source type="image/webp" media="(min-width: 769px)" srcset="/images/singl_Fashion_350.webp">
+                        <source type="image/webp" media="(min-width: 461px)" srcset="/images/singl_Fashion_470.webp">
+                        <source type="image/webp" srcset="/images/singl_Fashion_400.webp">
+                        <img src="/images/singl_Fashion.jpg" loading="lazy" alt="">
+                      </picture>
+
+                      <!--p class="en_content__releases__title">Fashion</p-->
+
+                      <p class="en_content__releases__subtitle">single &bull; 2023</p>
+
+                    </div>
+
+                  </a>
+
+                </div>
+
+                <div class="slick-slid">
+
+                  <a class="en_content__releases__anuncio" href="https://orcd.co/bm_model" target="_blank" title="">
+
+                    <div class="en_content__releases__heading">
+
+                      <picture class="en_content__releases__foto music">
+                        <source type="image/webp" media="(min-width: 1401px)" srcset="/images/singl_MODE-L_380.webp">
+                        <source type="image/webp" media="(min-width: 1201px)" srcset="/images/singl_MODE-L_400.webp">
+                        <source type="image/webp" media="(min-width: 1025px)" srcset="/images/singl_MODE-L_320.webp">
+                        <source type="image/webp" media="(min-width: 769px)" srcset="/images/singl_MODE-L_350.webp">
+                        <source type="image/webp" media="(min-width: 461px)" srcset="/images/singl_MODE-L_470.webp">
+                        <source type="image/webp" srcset="/images/singl_MODE-L_400.webp">
+                        <img src="/images/singl_MODE-L.jpg" loading="lazy" alt="">
+                      </picture>
+
+                      <!--p class="en_content__releases__title">MODE-L</p-->
+
+                      <p class="en_content__releases__subtitle">single &bull; 2022</p>
+
+                    </div>
+
+                  </a>
+
+                </div>
+
+                <div class="slick-slid">
+
+                  <a class="en_content__releases__anuncio" href="https://youtu.be/MZQnPUtyj2A" target="_blank" title="">
+
+                    <div class="en_content__releases__heading">
+
+                      <picture class="en_content__releases__foto music">
+                        <source type="image/webp" media="(min-width: 1401px)" srcset="/images/singl_Everyone_380.webp">
+                        <source type="image/webp" media="(min-width: 1201px)" srcset="/images/singl_Everyone_400.webp">
+                        <source type="image/webp" media="(min-width: 1025px)" srcset="/images/singl_Everyone_320.webp">
+                        <source type="image/webp" media="(min-width: 769px)" srcset="/images/singl_Everyone_350.webp">
+                        <source type="image/webp" media="(min-width: 461px)" srcset="/images/singl_Everyone_470.webp">
+                        <source type="image/webp" srcset="/images/singl_Everyone_400.webp">
+                        <img src="/images/singl_Everyone.jpg" loading="lazy" alt="">
+                      </picture>
+
+                      <!--p class="en_content__releases__title">Everyone (I’m Stuck)</p-->
+
+                      <p class="en_content__releases__subtitle">single &bull; 2022</p>
+
+                    </div>
+
+                  </a>
+
+                </div>
+
+              </div>
+            
+            </div>
+
+          </div>
+
+          <div class="en_content__releases__video">
+
+            <div class="en_content__releases__slogan"><p>Video</p></div>
+
+            <div class="en_content__releases__items">
+            
+              <div id="en_video">
+
+                <div class="slick-slid">
+
+                  <a class="en_content__releases__anuncio" href="https://youtu.be/MZQnPUtyj2A" target="_blank" title="">
+
+                    <div class="en_content__releases__heading">
+
+                      <picture class="en_content__releases__foto video">
+                        <source type="image/webp" media="(min-width: 1201px)" srcset="/images/video_MODE-L_820.webp">
+                        <source type="image/webp" media="(min-width: 1025px)" srcset="/images/video_MODE-L_720.webp">
+                        <source type="image/webp" media="(min-width: 769px)" srcset="/images/video_MODE-L_980.webp">
+                        <source type="image/webp" media="(min-width: 531px)" srcset="/images/video_MODE-L_720.webp">
+                        <source type="image/webp" media="(min-width: 461px)" srcset="/images/video_MODE-L_h320.webp">
+                        <source type="image/webp" srcset="/images/video_MODE-L_h270.webp">
+                        <img src="/images/video_MODE-L.jpg" loading="lazy" alt="">
+                      </picture>
+
+                      <p class="en_content__releases__subtitle"><b>MODE-L</b> &bull; 2022</p>
+
+                    </div>
+
+                  </a>
+
+              </div>
+
+                <div class="slick-slid">
+
+                  <a class="en_content__releases__anuncio" href="https://youtu.be/i-WuMFL08Ew" target="_blank" title="">
+
+                    <div class="en_content__releases__heading">
+
+                      <picture class="en_content__releases__foto video">
+                        <source type="image/webp" media="(min-width: 1201px)" srcset="/images/video_IM-STUCK_820.webp">
+                        <source type="image/webp" media="(min-width: 1025px)" srcset="/images/video_IM-STUCK_720.webp">
+                        <source type="image/webp" media="(min-width: 769px)" srcset="/images/video_IM-STUCK_980.webp">
+                        <source type="image/webp" media="(min-width: 531px)" srcset="/images/video_IM-STUCK_720.webp">
+                        <source type="image/webp" media="(min-width: 461px)" srcset="/images/video_IM-STUCK_h320.webp">
+                        <source type="image/webp" srcset="/images/video_IM-STUCK_h270.webp">
+                        <img src="/images/video_IM-STUCK.jpg" loading="lazy" alt="">
+                      </picture>
+
+                      <p class="en_content__releases__subtitle"><b>Everyone (I’m Stuck)</b> &bull; 2022</p>
+
+                    </div>
+
+                  </a>
+
+                </div>
+
+            </div>
+            
+            </div>
+
+          </div>
+        
+        </div>
+    
+      </div>
+        
+    </div>
+    
+    <div id="merch" class="en_content__merch bg_frontera_dos">
+
+      <div class="en_content__merch__content">
+
+        <div class="en_content__merch__title">
+        
+          <h1 class="en_content__title">Merch</h1>
+          
+          <p class="en_content__merch__subtitle">Coming soon</p>
+        
+        </div>
+        
+      </div>
+
+      <div class="en_content__merch__bg">
+
+        <picture class="en_content__merch__bg__foto">
+          <source type="image/webp" media="(min-width: 1025px)" srcset="/images/foto_merch_900.webp">
+          <source type="image/webp" media="(min-width: 769px)" srcset="/images/foto_merch_660.webp">
+          <source type="image/webp" srcset="/images/foto_merch_420.webp">
+          <img src="/images/foto_merch_900.jpg" loading="lazy" alt="">
+        </picture>
+      
+      </div>
+    
+    </div>
+    
+  </main>
+  
+  <footer id="kontakty" class="en_footer bg_frontera_uno">
+    
+    <div class="en_footer__content">
+
+      <div class="en_footer__about">
+
+        <div class="en_footer__about__text">
+        
+          <p class="en_footer__about__text__title">Official website of Britney Manson</p>
+          
+          <p class="en_footer__about__text__text">Here you can find out more about Britney Manson, see a poster and buy official merchandise. On the website, you can buy tickets, see the current Britney Manson concert and tour schedules.</p>
+          
+        </div>
+        
+        <div class="en_footer__about__socseti">
+
+          <a class="en_footer__about__socset__link tiktok_blanco" href="https://tiktok.com/@britneymnsn" target="_blank" title=""></a>
+
+          <a class="en_footer__about__socset__link instagram_blanco" href="https://instagram.com/britneymnsn" target="_blank" title=""></a>
+
+          <a class="en_footer__about__socset__link youtube_blanco" href="https://youtube.com/@britneymanson." target="_blank" title=""></a>
+
+        </div>
+
+      </div>
+
+      <div class="en_footer__kontakty">
+      
+      <p class="en_footer__kontakty__text">
+      
+        Management and concert tour promotion&nbsp;&mdash; <b>Merkaba Century, LLC</b><br>
+        Järvevana tee 9, Tallinn, 11314, Estonia<br>
+        <a class="en_phone" href="tel:+972545850335" title="">+972 54 5850335</a><br>
+        <a href="&#109;a&#105;&#108;To&#58;ceo&#64;merkabacentury&#46;com" title="">ceo&#64;merkabacentury&#46;com</a><br><br>
+        
+        <b>For questions about purchased tickets, for organizational and common issues:</b> <a href="&#109;a&#105;&#108;To&#58;business&#64;britneymanson&#46;com" title="">business&#64;britneymanson&#46;com</a>
+        
+      </p>
+
+      </div>
+
+    </div>
+  
+  </footer>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/include/js/js.htm"; ?>
+
+<div class="en_up"><div></div></div>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/images/bg_frontera_uno_grande.svg"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/images/bg_frontera_uno_medio.svg"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/images/bg_frontera_uno_min.svg"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/images/bg_frontera_dos_grande.svg"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/images/bg_frontera_dos_medio.svg"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/images/bg_frontera_dos_min.svg"; ?>
+
+</body>
+</html>
